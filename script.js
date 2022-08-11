91edc30ba6fef5d0417562779bd7cb3f28a0b8af
7-Astral <99803188+7-Astral@users.noreply.github.com>
2022-08-11 13:36:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let navbar=document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+};
+let cartItem=document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+};
+
+let searchForm=document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+};
